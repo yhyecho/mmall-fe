92f921d7af37107d81ac8feba8ade1aa6453c1b5
Echo <yhyecho@gmail.com>
2017-09-29 13:18:14 +0800

    add login register reset-password code

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,6 +1,16 @@
 var _mm = require('util/mm.js');
 
 var _user = {
+    // 用户登录
+    login: function(userInfo, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/user/login.do'),
+            data: userInfo,
+            method: 'POST',
+            success: resolve,
+            error: reject
+        });
+    },
     // 检查登录状态
     checkLogin: function(resolve, reject) {
         _mm.request({
@@ -10,10 +20,65 @@ var _user = {
             error: reject
         });
     },
+    // 获取密码提示问题
+    getQuestion: function(username, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/user/forget_get_question.do'),
+            data: {
+                username: username
+            },
+            method: 'POST',
+            success: resolve,
+            error: reject
+        });
+    },
+    // 检查密码提示问题答案
+    checkAnswer: function(userInfo, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/user/forget_check_answer.do'),
+            data: userInfo,
+            method: 'POST',
+            success: resolve,
+            error: reject
+        });
+    },
+    // 重置密码
+    resetPassword: function(userInfo, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/user/forget_reset_password.do'),
+            data: userInfo,
+            method: 'POST',
+            success: resolve,
+            error: reject
+        });
+    },
     // 登出
     logout: function(resolve, reject) {
         _mm.request({
             url: _mm.getServerUrl('/user/logout.do'),
+            method: 'POST',
+            success: resolve,
+            error: reject
+        });
+    },
+    // 检查用户名
+    checkUsername: function(username, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/user/check_valid.do'),
+            data: {
+                type: 'username',
+                str: username
+            },
+            method: 'POST',
+            success: resolve,
+            error: reject
+        });
+    },
+     // 用户注册
+     register: function(userInfo, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/user/register.do'),
+            data: userInfo,
             method: 'POST',
             success: resolve,
             error: reject
